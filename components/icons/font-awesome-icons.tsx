4f54a28de38f24e1339469facced8b9026eb8af8
clientesweb <alexisifran32@gmail.com>
2025-04-02 19:01:57 -0300

    Add files via upload

---
+++ components/icons/font-awesome-icons.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faYoutube, faInstagram, faSpotify, faTiktok } from "@fortawesome/free-brands-svg-icons"
+import {
+  faEnvelope,
+  faMapMarkerAlt,
+  faPhone,
+  faPlay,
+  faPause,
+  faVolumeUp,
+  faVolumeMute,
+  faExternalLinkAlt,
+  faHeadphones,
+  faCalendar,
+  faEye,
+  faThumbsUp,
+  faClock,
+} from "@fortawesome/free-solid-svg-icons"
+
+// Evitar que Font Awesome añada su CSS automáticamente
+config.autoAddCss = false
+
+// Exportar los iconos para facilitar su uso
+export {
+  FontAwesomeIcon,
+  faYoutube,
+  faInstagram,
+  faSpotify,
+  faTiktok,
+  faEnvelope,
+  faMapMarkerAlt,
+  faPhone,
+  faPlay,
+  faPause,
+  faVolumeUp,
+  faVolumeMute,
+  faExternalLinkAlt,
+  faHeadphones,
+  faCalendar,
+  faEye,
+  faThumbsUp,
+  faClock,
+}
+
